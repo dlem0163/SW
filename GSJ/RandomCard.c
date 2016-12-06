f662f001506d2bc967cf7e50c2973728813fd7b1
GSJ <dlem0163@naver.com>
2016-12-05 20:08:54 -0800

    connect Random in GUI

---
+++ GSJ/RandomCard.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> // srand
+
+char *card[16] = { "1","1","2","2","3","3","4","4","5","5","6","6","7","7","8","8" };
+int pick[16] = { 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1 };
+char *array[4][4] = { 0, };
+
+
+void Random_Setting()
+{
+	int i, j, temp;
+	int cnt = 0;
+
+	srand(time(NULL));
+
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			while (1)
+			{
+				temp = rand() % 16;
+				if (pick[temp] == 1)
+				{
+					array[i][j] = card[temp];
+					pick[temp] = 0;
+					break;
+				}
+			}
+		}
+	}
+	
+	for (i = 0; i < 4; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("%s ", array[i][j]);
+		}
+		printf("\n");
+	}
+	return ;
+}
+
